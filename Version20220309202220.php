05fec6cfc6c99c4fab4148ed1697460b6fa9fbf0
nhathoa2002 <142466074+nhathoa2002@users.noreply.github.com>
2023-08-17 09:49:00 +0700

    Add files via upload

---
+++ Version20220309202220.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220309202220 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE product_id');
+        $this->addSql('ALTER TABLE user ADD gender VARCHAR(255) NOT NULL, ADD phonenumber VARCHAR(255) NOT NULL, ADD gmail VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE product_id (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE user DROP gender, DROP phonenumber, DROP gmail');
+    }
+}
